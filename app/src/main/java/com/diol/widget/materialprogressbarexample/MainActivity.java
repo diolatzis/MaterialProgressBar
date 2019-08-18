c2b176474a72e26f894d373ba72c20523d4a9316
Stavros Diolatzis <sc17sd@leeds.ac.uk>
2019-08-18 18:58:19 +0300

    -Delayed animation to give time to views to be set

--- app/src/main/java/com/diol/widget/materialprogressbarexample/MainActivity.java
+++ app/src/main/java/com/diol/widget/materialprogressbarexample/MainActivity.java
@@ -3,11 +3,19 @@ package com.diol.widget.materialprogressbarexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.diol.widget.materialprogressbar.MaterialPercentageProgressBar;
+
 public class MainActivity extends AppCompatActivity {
 
+    MaterialPercentageProgressBar progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progress = findViewById(R.id.mppbProgress);
+
+        progress.fillTo(4);
+
     }
 }
